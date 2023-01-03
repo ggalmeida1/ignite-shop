956914203c4b6f5cf4eac27805a6f3f965e1620f
Gabriel Almeida <ggalmeida1@users.noreply.github.com>
2023-01-03 16:03:37 -0300

    Adding Styles via stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,13 @@
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
